74de55bfee94385ea65fd2967e1161a658edb97b
Orinameh <memefacto@yahoo.com>
2018-05-01 09:16:36 +0100

    added spec for keypad

--- src/components/KeyPad/KeyPad.jsx
+++ src/components/KeyPad/KeyPad.jsx
@@ -2,8 +2,29 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const KeyPad = ({callOperator, numbers, operators, setOperator, updateDisplay}) => {
-    return (<div className="keypad-container" />
-  );
+
+    const nums = numbers.map(number => {
+      return (
+        <p key={number}>{number}</p>
+      );
+    });
+
+    const ops = operators.map(operator => {
+      return (
+        <p key={operator}>{operator}</p>
+      );
+    });
+
+    return(
+      <div className="keypad-container">
+        <div className='numbers-container'>
+          {nums}
+        </div>
+        <div className="operators-container">
+          {ops}
+        </div>
+      </div>
+    );
 }
 
 KeyPad.propTypes = {
